7ac1ae314b43190ef069ee95d500029cae2eb356
Nitori <nitori@ikazuchi.cn>
2020-11-12 02:47:28 +0800

    predict

--- epicteller/bot/plugin/predict.py
+++ epicteller/bot/plugin/predict.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import base64
+import struct
+from random import random
+
+from nonebot import on_command, permission, CommandSession
+
+
+@on_command('p', permission=permission.PRIVATE_FRIEND)
+async def predict(session: CommandSession):
+    feed = [random.randbits(32) for _ in range(624)]
+    packed_feed = struct.pack('624I', feed)
+    b64_feed = base64.b64encode(packed_feed)
+    await session.send(b64_feed)
