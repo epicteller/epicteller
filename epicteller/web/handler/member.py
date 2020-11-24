b858d227d0c9fb97612918f20884a85ea677ce28
lizhibo <lizhibo@zhihu.com>
2020-11-24 20:38:13 +0800

    add sendcloud api

--- epicteller/web/handler/member.py
+++ epicteller/web/handler/member.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from fastapi import APIRouter, Depends
+
+from epicteller.core.model.member import Member
+from epicteller.web.controller.auth import get_current_member
+
+router = APIRouter()
+
+
+@router.get('/me')
+async def me(member: Member = Depends(get_current_member)):
+    return member
--- epicteller/web/worker/email.py
+++ epicteller/web/worker/email.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
 
-async def send_email(email: str, content: str):
-    pass
+import httpx
+
+from epicteller.core.config import Config
+
+
+async def send_register_email(email: str, token: str):
+    body = {
+        'apiUser': Config.SENDCLOUD_API_USER,
+        'apiKey': Config.SENDCLOUD_API_KEY,
+        'from': 'mailgun@epicteller.com',
+        'fromName': 'Epicteller邮件验证',
+        'templateInvokeName': 'epicteller_register',
+        'xsmtpapi': json.dumps({
+            'to': [email],
+            'sub': {'%token%': [token]},
+        }),
+    }
+    async with httpx.AsyncClient() as http_client:
+        r = await http_client.post(
+            'https://api.sendcloud.net/apiv2/mail/sendtemplate',
+            data=body,
+        )
+        r.raise_for_status()
