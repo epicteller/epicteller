cce55b85b28d2788ee935e133dfb0288454ee6be
Nitori <mdcq2005zgdh@gmail.com>
2021-01-14 20:22:06 +0800

    add core

--- epicteller/web/app.py
+++ epicteller/web/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from epicteller.core import redis
 from epicteller.web import bus_init
@@ -17,3 +18,10 @@ async def startup():
 app.include_router(combat.router, prefix='/combats')
 app.include_router(member.router)
 app.include_router(auth.router, prefix='/auth')
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["http://localhost",
+                                  "https://*.epicteller.com"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
