212675c1a1f8ba07a87ca5a264e221b73a73ebdd
Nitori <mdcq2005zgdh@gmail.com>
2021-01-14 21:40:00 +0800

    combat link

--- epicteller/bot/controller/combat.py
+++ epicteller/bot/controller/combat.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from nonebot.adapters.cqhttp import escape
+
 from epicteller.core.controller import character as character_ctl
-from epicteller.core.model.combat import CombatToken
+from epicteller.core.model.combat import CombatToken, Combat
 from nonebot.adapters.cqhttp.message import Message, MessageSegment
 
 from epicteller.core.util.enum import ExternalType
@@ -20,3 +22,10 @@ async def format_token_message(token: CombatToken) -> Message:
     if character.name == token.name:
         return Message(MessageSegment.at(external_id))
     return Message(f'「{token.name}」(控制者：{MessageSegment.at(external_id)})')
+
+
+async def format_combat_xml(combat: Combat) -> str:
+    data = r'''
+    <?xml version='1.0' encoding='UTF-8' standalone='yes' ?><msg serviceID="1" templateID="123" action="web" brief="[战斗] 战斗状态" sourceMsgId="0" url="https://www.epicteller.com/combat/{url_token}" flag="0" adverSign="0" multiMsgFlag="0"><item layout="2" advertiser_id="0" aid="0"><picture cover="https://ep-predict.epicteller.com/icon512.png" w="0" h="0" /><title>战斗状态</title><summary>战斗状态</summary></item><source name="" icon="" action="" appid="-1" /></msg>
+        '''.format(url_token=combat.url_token)
+    return escape(data)
